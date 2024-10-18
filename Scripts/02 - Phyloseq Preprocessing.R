ee5326a3f249d9b81acd2ceb496e5461c5c74b80
pattonsunni <pattonsu@oregonstate.edu>
2024-10-18 10:25:45 -0700

    Update 02 - Phyloseq Preprocessing

--- Scripts/02 - Phyloseq Preprocessing.R
+++ Scripts/02 - Phyloseq Preprocessing.R
@@ -1,5 +1,5 @@
 # Author: Sunni Patton
-# Last edited: 7/10/24
+# Last edited: 10/18/24 
 # Title: Creating initial phyloseq object
 
 ## Set seed ====
@@ -9,18 +9,18 @@ set.seed(123)
 library(phyloseq)
 
 ## Load metadata file ====
-read.csv(here::here("Output Files/01 - Metadata - Output/metadata_quant.csv")) -> metadata_quant
+read.csv(here::here("Output Files/01 - Metadata - Output/metadata.csv")) -> metadata
 
 ## Load sequence table and taxonomy table ====
 readRDS(here::here("Output Files/00 - Read Preprocessing - Output/seq_table_noNA.rds")) -> seq_table_noNA
 readRDS(here::here("Output Files/00 - Read Preprocessing - Output/taxonomy_noNA.rds")) -> taxonomy_noNA
 
 ## Make sample numbers into sample names to work with phyloseq ====
-x <- metadata_quant$Samples
-rownames(metadata_quant) <- x
+x <- metadata$Samples
+rownames(metadata) <- x
 
 ## Create phyloseq object ====
-ps.All <- phyloseq(otu_table(seq_table_noNA, taxa_are_rows=FALSE), sample_data(metadata_quant), tax_table(taxonomy_noNA))
+ps.All <- phyloseq(otu_table(seq_table_noNA, taxa_are_rows=FALSE), sample_data(metadata), tax_table(taxonomy_noNA))
 saveRDS(ps.All, here::here("Output Files/02 - Phyloseq Preprocessing - Output/ps.All.rds"))
 
 ## Inspect phyloseq object ====
@@ -29,4 +29,3 @@ ps.All
 summary(taxa_sums(ps.All@otu_table)) 
 # Sample read distribution
 summary(sample_sums(ps.All@otu_table)) 
-
